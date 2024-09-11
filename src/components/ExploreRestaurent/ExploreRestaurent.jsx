2282949f6871055e883def1697797dbfbbe60361
Sarathkumarpk123 <129288412+Sarathkumarpk123@users.noreply.github.com>
2024-09-11 09:01:03 +0530

    Add files via upload

---
+++ src/components/ExploreRestaurent/ExploreRestaurent.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './ExploreRestaurent.css'
+const ExploreRestaurent = () => {
+  return (
+    <div>
+      <h1>Explore Our Top Restaurents</h1>
+      <p className='restcontent'>The restaurant seemed magical and fascinating to me when I was an elementary school kid, and surprisingly, the place still fascinates me today.</p>
+      
+    </div>
+  )
+}
+
+export default ExploreRestaurent
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Home.css'
+import Header from '../../components/Header/Header'
+import ExploreRestaurent from '../../components/ExploreRestaurent/ExploreRestaurent'
+const Home = () => {
+  return (
+    <div>
+      <Header />
+      <ExploreRestaurent/>
+    </div>
+  )
+}
+
+export default Home
